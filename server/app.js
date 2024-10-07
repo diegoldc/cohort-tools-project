591d055cf716cb98204df10737d21d39cf74870c
Diego Lázaro <dlazarodecastro@gmail.com>
2024-10-07 16:39:01 +0200

    day 1 - express project

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-
+const cohorts = require("./data/cohorts.json")
+const students = require("./data/students.json")
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -15,12 +17,14 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+app.use(cors({
+  origin: ["http://localhost:5173"]
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
@@ -28,6 +32,14 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
+
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts)
+})
+
+app.get("/api/students", (req, res) => {
+  res.json(students)
+})
 
 
 // START SERVER
